5b3979087caf1ce57a7ce61deed497f2549ef262
Jo Vianto <sjovianto@gmail.com>
2024-08-06 15:53:43 +0900

    change image logger

--- VQ-GAN/main.py
+++ VQ-GAN/main.py
@@ -217,65 +217,65 @@ class SetupCallback(Callback):
                 except FileNotFoundError:
                     pass
 
-
 class ImageLogger(Callback):
-    def __init__(self, batch_frequency, max_images, clamp=True, increase_log_steps=True):
+    def __init__(self, batch_frequency, max_images, clamp=True, increase_log_steps=True,
+                 rescale=True, disabled=False, log_on_batch_idx=False, log_first_step=False,
+                 log_images_kwargs=None, save_dir=None):
         super().__init__()
+        self.rescale = rescale
         self.batch_freq = batch_frequency
         self.max_images = max_images
-        self.logger_log_images = {
-            pl.loggers.WandbLogger: self._wandb,
-            pl.loggers.CSVLogger: self._testtube,
-        }
+        self.save_dir = save_dir
+        # self.logger_log_images = {
+        #     pl.loggers.TestTubeLogger: self._testtube,
+        # }
         self.log_steps = [2 ** n for n in range(int(np.log2(self.batch_freq)) + 1)]
         if not increase_log_steps:
             self.log_steps = [self.batch_freq]
         self.clamp = clamp
-
-    @rank_zero_only
-    def _wandb(self, pl_module, images, batch_idx, split):
-        raise ValueError("No way wandb")
-        grids = dict()
-        for k in images:
-            grid = torchvision.utils.make_grid(images[k])
-            grids[f"{split}/{k}"] = wandb.Image(grid)
-        pl_module.logger.experiment.log(grids)
+        self.disabled = disabled
+        self.log_on_batch_idx = log_on_batch_idx
+        self.log_images_kwargs = log_images_kwargs if log_images_kwargs else {}
+        self.log_first_step = log_first_step
 
     @rank_zero_only
     def _testtube(self, pl_module, images, batch_idx, split):
-        return
-        # for k in images:
-        #     grid = torchvision.utils.make_grid(images[k])
-        #     grid = (grid+1.0)/2.0 # -1,1 -> 0,1; c,h,w
+        for k in images:
+            grid = torchvision.utils.make_grid(images[k])
+            grid = (grid + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
 
-        #     tag = f"{split}/{k}"
-            # pl_module.logger.experiment.add_image(
-            #     tag, grid,
-            #     global_step=pl_module.global_step)
+            tag = f"{split}/{k}"
+            pl_module.logger.experiment.add_image(
+                tag, grid,
+                global_step=pl_module.global_step)
 
     @rank_zero_only
-    def log_local(self, save_dir, split, images,
-                  global_step, current_epoch, batch_idx):
-        root = os.path.join(save_dir, "images", split)
+    def log_local_online(self, split, images,
+                  global_step, current_epoch, batch_idx, logger):
+        root = os.path.join(self.save_dir, "images", split)
         for k in images:
-            img = images[k].squeeze(0)  # remove batch dimension, now it's (1, 192, 192, 160)
-            img = img.permute(1, 2, 3, 0)  # reorder dimensions to be compatible with nibabel
-
-            img = img.numpy()
-
-            filename = "{}_gs-{:06}_e-{:06}_b-{:06}.nii.gz".format(
+            grid = torchvision.utils.make_grid(images[k], nrow=2)
+            if self.rescale:
+                grid = (grid + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
+            grid = grid.transpose(0, 1).transpose(1, 2).squeeze(-1)
+            grid = grid.numpy()
+            grid = (grid * 255.).astype(np.uint8)
+            filename = "{}_gs-{:06}_e-{:06}_b-{:06}.png".format(
                 k,
                 global_step,
                 current_epoch,
                 batch_idx)
             path = os.path.join(root, filename)
             os.makedirs(os.path.split(path)[0], exist_ok=True)
-
-            nifti_img = nib.Nifti1Image(img, np.eye(4))  # you might want to replace np.eye(4) with the correct affine matrix
-            nib.save(nifti_img, path)
-
+            image = Image.fromarray(grid)
+            image.save(path)
+            try:
+                logger.experiment.log_image(image, name=f"{split}_{filename}")
+            except:
+                pass
     def log_img(self, pl_module, batch, batch_idx, split="train"):
-        if (self.check_frequency(batch_idx) and  # batch_idx % self.batch_freq == 0
+        check_idx = batch_idx if self.log_on_batch_idx else pl_module.global_step
+        if (self.check_frequency(check_idx) and  # batch_idx % self.batch_freq == 0
                 hasattr(pl_module, "log_images") and
                 callable(pl_module.log_images) and
                 self.max_images > 0):
@@ -286,7 +286,7 @@ class ImageLogger(Callback):
                 pl_module.eval()
 
             with torch.no_grad():
-                images = pl_module.log_images(batch, split=split, pl_module=pl_module)
+                images = pl_module.log_images(batch, split=split, **self.log_images_kwargs)
 
             for k in images:
                 N = min(images[k].shape[0], self.max_images)
@@ -296,29 +296,140 @@ class ImageLogger(Callback):
                     if self.clamp:
                         images[k] = torch.clamp(images[k], -1., 1.)
 
-            self.log_local(pl_module.logger.save_dir, split, images,
-                           pl_module.global_step, pl_module.current_epoch, batch_idx)
+            self.log_local_online(split, images,
+                           pl_module.global_step, pl_module.current_epoch, batch_idx, pl_module.logger)
 
-            logger_log_images = self.logger_log_images.get(logger, lambda *args, **kwargs: None)
-            logger_log_images(pl_module, images, pl_module.global_step, split)
+            # logger_log_images = self.logger_log_images.get(logger, lambda *args, **kwargs: None)
+            # logger_log_images(pl_module, images, pl_module.global_step, split)
 
             if is_train:
                 pl_module.train()
 
-    def check_frequency(self, batch_idx):
-        if (batch_idx % self.batch_freq) == 0 or (batch_idx in self.log_steps):
+    def check_frequency(self, check_idx):
+        if ((check_idx % self.batch_freq) == 0 or (check_idx in self.log_steps)) and (
+                check_idx > 0 or self.log_first_step):
             try:
-                self.log_steps.pop(0)
-            except IndexError:
+                if len(self.log_steps) > 0:
+                    self.log_steps.pop(0)
+            except IndexError as e:
+                print(e)
                 pass
             return True
         return False
 
-    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
-        self.log_img(pl_module, batch, batch_idx, split="train")
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx = 0):
+        if not self.disabled and (pl_module.global_step > 0 or self.log_first_step):
+            self.log_img(pl_module, batch, batch_idx, split="train")
 
-    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
-        self.log_img(pl_module, batch, batch_idx, split="val")
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx = 0):
+        if not self.disabled and pl_module.global_step > 0:
+            self.log_img(pl_module, batch, batch_idx, split="val")
+        # if hasattr(pl_module, 'calibrate_grad_norm'):
+        #     if (pl_module.calibrate_grad_norm and batch_idx % 25 == 0) and batch_idx > 0:
+        #         self.log_gradients(trainer, pl_module, batch_idx=batch_idx)
+
+
+# class ImageLogger(Callback):
+#     def __init__(self, batch_frequency, max_images, clamp=True, increase_log_steps=True):
+#         super().__init__()
+#         self.batch_freq = batch_frequency
+#         self.max_images = max_images
+#         self.logger_log_images = {
+#             pl.loggers.WandbLogger: self._wandb,
+#             pl.loggers.CSVLogger: self._testtube,
+#         }
+#         self.log_steps = [2 ** n for n in range(int(np.log2(self.batch_freq)) + 1)]
+#         if not increase_log_steps:
+#             self.log_steps = [self.batch_freq]
+#         self.clamp = clamp
+
+#     @rank_zero_only
+#     def _wandb(self, pl_module, images, batch_idx, split):
+#         raise ValueError("No way wandb")
+#         grids = dict()
+#         for k in images:
+#             grid = torchvision.utils.make_grid(images[k])
+#             grids[f"{split}/{k}"] = wandb.Image(grid)
+#         pl_module.logger.experiment.log(grids)
+
+#     @rank_zero_only
+#     def _testtube(self, pl_module, images, batch_idx, split):
+#         return
+#         # for k in images:
+#         #     grid = torchvision.utils.make_grid(images[k])
+#         #     grid = (grid+1.0)/2.0 # -1,1 -> 0,1; c,h,w
+
+#         #     tag = f"{split}/{k}"
+#             # pl_module.logger.experiment.add_image(
+#             #     tag, grid,
+#             #     global_step=pl_module.global_step)
+
+#     @rank_zero_only
+#     def log_local(self, save_dir, split, images,
+#                   global_step, current_epoch, batch_idx):
+#         root = os.path.join(save_dir, "images", split)
+#         for k in images:
+#             img = images[k].squeeze(0)  # remove batch dimension, now it's (1, 192, 192, 160)
+#             img = img.permute(1, 2, 3, 0)  # reorder dimensions to be compatible with nibabel
+
+#             img = img.numpy()
+
+#             filename = "{}_gs-{:06}_e-{:06}_b-{:06}.nii.gz".format(
+#                 k,
+#                 global_step,
+#                 current_epoch,
+#                 batch_idx)
+#             path = os.path.join(root, filename)
+#             os.makedirs(os.path.split(path)[0], exist_ok=True)
+
+#             nifti_img = nib.Nifti1Image(img, np.eye(4))  # you might want to replace np.eye(4) with the correct affine matrix
+#             nib.save(nifti_img, path)
+
+#     def log_img(self, pl_module, batch, batch_idx, split="train"):
+#         if (self.check_frequency(batch_idx) and  # batch_idx % self.batch_freq == 0
+#                 hasattr(pl_module, "log_images") and
+#                 callable(pl_module.log_images) and
+#                 self.max_images > 0):
+#             logger = type(pl_module.logger)
+
+#             is_train = pl_module.training
+#             if is_train:
+#                 pl_module.eval()
+
+#             with torch.no_grad():
+#                 images = pl_module.log_images(batch, split=split, pl_module=pl_module)
+
+#             for k in images:
+#                 N = min(images[k].shape[0], self.max_images)
+#                 images[k] = images[k][:N]
+#                 if isinstance(images[k], torch.Tensor):
+#                     images[k] = images[k].detach().cpu()
+#                     if self.clamp:
+#                         images[k] = torch.clamp(images[k], -1., 1.)
+
+#             self.log_local(pl_module.logger.save_dir, split, images,
+#                            pl_module.global_step, pl_module.current_epoch, batch_idx)
+
+#             logger_log_images = self.logger_log_images.get(logger, lambda *args, **kwargs: None)
+#             logger_log_images(pl_module, images, pl_module.global_step, split)
+
+#             if is_train:
+#                 pl_module.train()
+
+#     def check_frequency(self, batch_idx):
+#         if (batch_idx % self.batch_freq) == 0 or (batch_idx in self.log_steps):
+#             try:
+#                 self.log_steps.pop(0)
+#             except IndexError:
+#                 pass
+#             return True
+#         return False
+
+#     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+#         self.log_img(pl_module, batch, batch_idx, split="train")
+
+#     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+#         self.log_img(pl_module, batch, batch_idx, split="val")
 
 
 
